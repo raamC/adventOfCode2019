7aa337900a6217867e151a58a84717c266881c80
raamC <raamrajchauhan@gmail.com>
2020-02-17 21:15:45 +0000

    Day 1

---
+++ day1.ts
@@ -0,0 +1,29 @@
+var fs = require("fs");
+var data = fs.readFileSync("./day1_input1.txt", 'utf-8');
+data = data.trim().split('\n')
+
+function getFuelRequired(mass: number): number {
+    return Math.floor(mass/3) - 2
+}
+
+var fuel = data.map(x => getFuelRequired(x))
+var totalFuel = fuel.reduce((a, b) => a + b, 0)
+console.log(`Part 1: ${totalFuel}`)
+
+function getTotalFuel(mass: number): number {
+    var totalFuel = 0
+    var fuel = getFuelRequired(mass);
+    totalFuel = totalFuel + fuel
+
+    while(getFuelRequired(fuel) > 0) {
+        fuel = getFuelRequired(fuel)
+        totalFuel = totalFuel + fuel
+    }
+
+    return totalFuel
+
+}
+
+var fuel = data.map(x => getTotalFuel(x))
+var totalFuel = fuel.reduce((a, b) => a + b, 0)
+console.log(`Part 2: ${totalFuel}`)
